15969cc16d65784807a23056eca6c1de66ac3a8e
AnkitBhatt-2004 <ankitbhatt2212@gmail.com>
2023-05-18 09:27:18 +0530

    push commit

--- deletion.java
+++ deletion.java
@@ -1,7 +1,7 @@
     import  java.util.Scanner;
 public class deletion {
 
-    public static void main(String args[]) {
+     static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     int i,j,n;
     System.out.println("Enter no of element is array");
